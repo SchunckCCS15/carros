a53df25c154e81e462181ea386d7c225dc6482d6
Cauã Schunck <branquelinho.schunck@gmail.com>
2024-03-30 16:33:09 -0300

    ultimo commit da responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,19 @@ export default function App () {
 
     <main className= "min-h-svh bg-offwhitebg py-20 px-6 antialiased lg:flex justify-center items-center" >
       
-      <section className= "lg:flex" >
+      <section className= "lg:flex max-w-[920px]" >
         
 
 
-        <div className= "bg-orange p-12 rounded-t-lg" >
+        <div className= "bg-orange p-12 max-lg:rounded-t-lg lg:rounded-l-lg" >
           
           <img src={iconSedans} alt= "ícone dos carros sedan" className="mb-7" />
           
           <h2 className= "font-shoulders uppercase text-4xl font-bold text-offwhitebg mb-7" >Sedans</h2>
           
-          <p className= "font-lexend text-offwhite mb-7" >Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
+          <p className= "font-lexend text-offwhite mb-7 lg:mb-20" >Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
           
-          <a className= "font-lexend bg-offwhitebg text-orange block rounded-3xl px-10 py-3 w-fit" href= "https://www.webmotors.com.br/sedans" target="_blanck" >Ver mais</a>
+          <a className= "font-lexend bg-offwhitebg border-offwhitebg text-orange block rounded-3xl px-10 py-3 w-fit border-2 hover:bg-orange hover:text-offwhitebg" href= "https://www.webmotors.com.br/sedans" target="_blanck" >Ver mais</a>
         
         </div>
 
@@ -32,23 +32,23 @@ export default function App () {
 
           <h2 className= "font-shoulders uppercase text-4xl font-bold text-offwhitebg mb-7" >SUVs</h2>
 
-          <p className= "font-lexend text-offwhite mb-7" >Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
+          <p className= "font-lexend text-offwhite mb-7 lg:mb-20" >Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
 
-          <a className= "font-lexend bg-offwhitebg text-green block rounded-3xl px-10 py-3 w-fit" href="https://www.webmotors.com.br/suvs" target="_blanck" >Ver mais</a>
+          <a className= "font-lexend bg-offwhitebg border-offwhitebg text-green block rounded-3xl px-10 py-3 w-fit border-2 hover:bg-green hover:text-offwhitebg" href="https://www.webmotors.com.br/suvs" target="_blanck" >Ver mais</a>
 
         </div>
 
 
 
-        <div className= "bg-dark-green p-12 rounded-b-lg" >
+        <div className= "bg-dark-green p-12 max-lg:rounded-b-lg lg:rounded-r-lg" >
 
           <img src={iconLuxury} alt= "ícone dos carros de luxo" className= "mb-7" />
 
           <h2 className= "font-shoulders uppercase text-4xl font-bold text-offwhitebg mb-7" >Luxuosos</h2>
 
-          <p className= "font-lexend text-offwhite mb-7" >Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
+          <p className= "font-lexend text-offwhite mb-7 lg:mb-20" >Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
 
-          <a className= "font-lexend bg-offwhitebg text-dark-green block rounded-3xl px-10 py-3 w-fit" href="https://www.webmotors.com.br/carros-de-luxo" target="_blanck" >Ver mais</a>
+          <a className= "font-lexend bg-offwhitebg border-offwhitebg text-dark-green block rounded-3xl px-10 py-3 w-fit border-2 hover:bg-dark-green hover:text-offwhitebg" href="https://www.webmotors.com.br/carros-de-luxo" target="_blanck" >Ver mais</a>
 
         </div>
       
